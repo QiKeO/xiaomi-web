90dda4c26f8a6f591139ebcbb0af3d0b70184c92
柒刻 <110709659+QiKeO@users.noreply.github.com>
2025-03-02 20:42:39 +0800

    Add files via upload
    配置文件

--- config.js
+++ config.js
@@ -3,24 +3,6 @@
  * @Date: 2025-02-22 23:18:06
  * @LastEditTime: 2025-02-26 12:19:20
  * @FilePath: \xiaomi\config.js
- * 
- * 有趣的密码提示语收集：
- * 1. "你猜我有没有密码(｀・ω・´)"
- * 2. "密码？不存在的！(๑•̀ㅂ•́)و✧"
- * 3. "这个密码很神秘哦(●ˇ∀ˇ●)"
- * 4. "偷偷告诉你，这是假的密码(｡◕‿◕｡)"
- * 5. "密码被猫吃了(ฅ´ω`ฅ)"
- * 6. "这哪有什么密码(￣▽￣)~*"
- * 7. "密码在月球上呢(∩_∩)/"
- * 8. "你看不见我，看不见我⊙▽⊙"
- * 9. "这是一个假装有密码的密码✧( •̀ ω •́ )✧"
- * 10. "密码正在度假中(～￣▽￣)～"
- * 11. "密码君表示不想出现╮(╯▽╰)╭"
- * 12. "密码？我也想知道(｀・ω・´)"
- * 13. "这里本来有个密码，但是它跑了(◍•ᴗ•◍)"
- * 14. "密码去追寻梦想了✿◡‿◡"
- * 15. "密码在另一个次元(｡♥‿♥｡)"
- * 
  * danger // 红色警告
  * warning  // 黄色警告
  * info  // 默认蓝色提示
@@ -34,22 +16,16 @@ const linksConfig = {
                 {
                     title: "小米工作台",
                     url: "https://asp.mi.com",
-                    username: "13453140073",
-                    password: "ww2391695405.+-",
                     warning: "首次登录需要进行手机验证"
                 },
                 {
                     title: "小米知识库",
                     url: "https://kbs.be.xiaomi.com",
-                    username: "13453140073",
-                    password: "ww2391695405.+-",
                     warning: "首次登录需要进行手机验证"
                 },
                 {
                     title: "小米POS",
                     url: "pos.be.xiaomi.com",
-                    username: "13453140073",
-                    password: "ww2391695405.+-",
                     warning: "首次登录需要进行手机验证"
                 }
             ]
@@ -61,15 +37,11 @@ const linksConfig = {
                 {
                     title: "北京税务系统",
                     url: "https://etax.beijing.chinatax.gov.cn:8443/loginb/",
-                    username: "13453140073",
-                    password: "ww2391695405.+-",
                     warning: "请使用Chrome浏览器访问，登录时请确保安全控件已安装"
                 },
                 {
                     title: "国补核销",
                     url: "http://xm.58woshop.com/beiJian_coupon/main.html",
-                    username: "15810621731",
-                    password: "1731yksj@1125",
                     warning: "国补核销管理系统"
                 }
             ]
@@ -81,15 +53,11 @@ const linksConfig = {
                 {
                     title: "京东到家商家版",
                     url: "https://store.jddj.com",
-                    username: "13453140073",
-                    password: "看啥呢，没有( •̀ ω •́ )✧",
                     warning: "商家后台管理系统"
                 },
                 {
                     title: "抖音商家后台",
                     url: "https://fxg.jinritemai.com/login",
-                    username: "13453140073",
-                    password: "二次加密了啦(●'◡'●)",
                     warning: "抖音电商商家后台"
                 }
             ]
